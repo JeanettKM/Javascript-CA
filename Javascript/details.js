fb290ce65d8e6e3b9b6644ce67b85a4ab58d992e
JeanettKM <JeaMel68404@stud.noroff.no>
2023-02-26 17:57:47 +0100

    added try and catch errors

--- Javascript/details.js
+++ Javascript/details.js
@@ -12,24 +12,30 @@ const urlDetails = "https://legacy--api.herokuapp.com/api/v1/characters/" + id;
 console.log
 
 async function charSpecific() {
-    const response = await fetch (urlDetails);
-    const details = await response.json();
-
-    console.log(details);
-
-    container.innerHTML = `<h1>${details.name}</h1>
+    try {
+        const response = await fetch (urlDetails);
+        const details = await response.json();
     
-    <div><li>
-    <ul>Date of birth: ${details.born}</ul>
-    <ul>Ancestery: ${details.blood}</ul>
-    <ul>Relationship status: ${details.marital}</ul>
-    <ul>nationality: ${details.nationality}</ul>
-    <ul>Loyalty: ${details.loyalty}</ul>
-    <ul>gender: ${details.gender}</ul>
-    <ul>image: ${details.image_url}</ul>
-    <ul>nationality: ${details.nationality}</ul>
-        </li></div>
-        <time class="details-date">Released: ${details.created_at}</time>`
+        console.log(details);
+    
+        container.innerHTML = `<h1>${details.name}</h1>
+        
+        <div><li>
+        <ul>Date of birth: ${details.born}</ul>
+        <ul>Ancestery: ${details.blood}</ul>
+        <ul>Relationship status: ${details.marital}</ul>
+        <ul>nationality: ${details.nationality}</ul>
+        <ul>Loyalty: ${details.loyalty}</ul>
+        <ul>gender: ${details.gender}</ul>
+        <ul>image: ${details.image_url}</ul>
+        <ul>nationality: ${details.nationality}</ul>
+            </li></div>
+            <time class="details-date">Released: ${details.created_at}</time>`
+    }
+    catch(error) {
+        console.log("error occurred", error);
+        container.innerHTML = "We're sorry, it seems a spell went bad, try again later or refresh the page(error).";
+    }
 
 }
 
--- Javascript/script.js
+++ Javascript/script.js
@@ -3,22 +3,24 @@ const url = "https://legacy--api.herokuapp.com/api/v1/characters";
 const resultsContainer = document.querySelector(".results")
 
 async function getCharacters() {
-        const response = await fetch (url);
-    
-        const results = await response.json();
-    
-        resultsContainer.innerHTML ="";
-    
-        const facts = results;
-    
-        facts.forEach(function(chars){
-    
-        resultsContainer.innerHTML += `<a href="/HTML/details.html?id=${chars.id}"><h3>${chars.name}</h3></a>`
-        });
-
-}
         
+        try {        const response = await fetch (url);
+    
+                const results = await response.json();
+            
+                resultsContainer.innerHTML ="";
+            
+                const facts = results;
+            
+                facts.forEach(function(chars){
+            
+                resultsContainer.innerHTML += `<a href="/HTML/details.html?id=${chars.id}"><h3>${chars.name}</h3></a>`
+                });}
+                catch(error) {
+                        console.log("error occured", error);
+                        resultsContainer.innerHTML = "We're sorry, it seems a spell went bad, try again later or refresh the page(error).";
+                }
+
+        }
 
 getCharacters();
-
-
